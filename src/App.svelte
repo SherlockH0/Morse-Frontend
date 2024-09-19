7c45f95067c7ec9ba1e1394d2e30c69a8253952e
SherlockH0 <ddmlf7@gmail.com>
2024-09-19 17:02:54 +0300

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+  import Navbar from "./lib/Navbar.svelte";
+  import Textinput from "./lib/Textinput.svelte";
+</script>
+
+<Navbar />
+<main>
+  <div class="p-2 pb-4">
+    <Counter />
+    <Textinput />
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/lib/Textinput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  // your script goes here
+  function onEnter(event: any) {
+    if (event.keyCode == 13 && !event.shiftKey) {
+      event.preventDefault();
+      document.forms[0].submit();
+    }
+  }
+</script>
+
+<form action="" id="form" class="flex gap-4">
+  <textarea
+    class="textarea textarea-bordered"
+    placeholder="Bio"
+    on:keypress={onEnter}
+    rows="3"
+  ></textarea>
+  <button type="submit" class="btn btn-primary">Send</button>
+</form>
